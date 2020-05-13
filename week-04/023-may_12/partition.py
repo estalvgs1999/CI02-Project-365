23170288713f8593d7e855922adfeaae6356f584
Esteban Alvarado <estalvgs1999@gmail.com>
2020-05-12 22:32:17 -0600

    Partition

---
+++ week-04/023-may_12/partition.py
@@ -0,0 +1,22 @@
+# ---------------------------------------
+#                  DAY 23
+#         Coding Interview Question
+#
+#                PARTITION
+#  Write code to partition a linked list
+#  around a value x, such that all nodes
+#  less than x come before all nodes
+#  greater than or equal to x. If x is
+#  contained within the list, the values
+#  of x only need to be after the
+#  elements less than x (see below).
+#  The partition element x can appear
+#  anywhere in the "right partition"; 
+#  it does not need to appear between
+#  the left and right partitions.
+#  
+#  Question 2.4 | ctci 6th edition
+#
+#  by: @estalvgs1999
+# ---------------------------------------
+
